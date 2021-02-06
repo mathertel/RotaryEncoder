9648e54f80e998c7f7807e5ca32189152ceb1b92
Matthias Hertel <mathertel@hotmail.com>
2021-02-06 11:07:40 +0100

    examples update

--- examples/InterruptRotator/InterruptRotator.ino
+++ examples/InterruptRotator/InterruptRotator.ino
@@ -1,63 +1,109 @@
 // -----
-// InterruptRotator.ino - Example for the RotaryEncoder library.
+// SimplePollRotator.ino - Example for the RotaryEncoder library.
 // This class is implemented for use with the Arduino environment.
+//
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// This work is licensed under a BSD 3-Clause License. See http://www.mathertel.de/License.aspx
 // More information on: http://www.mathertel.de/Arduino
 // -----
 // 18.01.2014 created by Matthias Hertel
+// 04.02.2021 conditions and settings added for ESP8266
 // -----
 
-// This example checks the state of the rotary encoder in the loop() function.
-// The current position is printed on output when changed.
+// This example checks the state of the rotary encoder using interrupts and in the loop() function.
+// The current position and direction is printed on output when changed.
 
 // Hardware setup:
-// Attach a rotary encoder with output pins to A2 and A3.
+// Attach a rotary encoder with output pins to
+// * 2 and 3 on Arduino UNO. (supported by attachInterrupt)
+// * A2 and A3 can be used when directly using the ISR interrupts, see comments below.
+// * D5 and D6 on ESP8266 board (e.g. NodeMCU).
+// Swap the pins when direction is detected wrong.
 // The common contact should be attached to ground.
+//
+// Hints for using attachinterrupt see https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
 
+#include <Arduino.h>
 #include <RotaryEncoder.h>
 
-// Setup a RoraryEncoder for pins A2 and A3:
-RotaryEncoder encoder(A2, A3);
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
+// Example for Arduino UNO with input signals on pin 2 and 3
+#define PIN_IN1 2
+#define PIN_IN2 3
+
+#elif defined(ESP8266)
+// Example for ESP8266 NodeMCU with input signals on pin D5 and D6
+#define PIN_IN1 D5
+#define PIN_IN2 D6
+
+#endif
+
+// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
+// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+
+// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+
+
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
+// This interrupt routine will be called on any change of one of the input signals
+void checkPosition()
+{
+  encoder.tick(); // just call tick() to check the state.
+}
+
+#elif defined(ESP8266)
+/**
+ * @brief The interrupt service routine will be called on any change of one of the input signals.
+ */
+ICACHE_RAM_ATTR void checkPosition()
+{
+  encoder.tick(); // just call tick() to check the state.
+}
+
+#endif
+
 
 void setup()
 {
-  Serial.begin(57600);
-  Serial.println("SimplePollRotator example for the RotaryEncoder library.");
+  Serial.begin(115200);
+  while (! Serial);
+  Serial.println("InterruptRotator example for the RotaryEncoder library.");
 
-  // You may have to modify the next 2 lines if using other pins than A2 and A3
-  PCICR |= (1 << PCIE1);    // This enables Pin Change Interrupt 1 that covers the Analog input pins or Port C.
-  PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);  // This enables the interrupt for pin 2 and 3 of Port C.
+  attachInterrupt(PIN_IN1, checkPosition, CHANGE);
+  attachInterrupt(PIN_IN2, checkPosition, CHANGE);
 } // setup()
-
-
-
-// The Interrupt Service Routine for Pin Change Interrupt 1
-// This routine will only be called on any signal change on A2 and A3: exactly where we need to check.
-ISR(PCINT1_vect) {
-  encoder.tick(); // just call tick() to check the state.
-}
 
 
 // Read the current position of the encoder and print out when changed.
 void loop()
 {
   static int pos = 0;
+  encoder.tick();
 
   int newPos = encoder.getPosition();
   if (pos != newPos) {
+    Serial.print("pos:");
     Serial.print(newPos);
-    Serial.println();
+    Serial.print(" dir:");
+    Serial.println((int)(encoder.getDirection()));
     pos = newPos;
-
-    // Just to show, that long lasting procedures don't break the rotary encoder:
-    // When newPos is 66 the ouput will freeze, but the turned positions will be recognized even when not polled.
-    // The interrupt still works.
-    // The output is correct 6.6 seconds later.
-    if (newPos == 66)
-      delay(6600);
   } // if
 } // loop ()
 
-// The End
 
+// To use other pins with Arduino UNO you can also use the ISR directly.
+// Here is some code for A2 and A3 using ATMega168 ff. specific registers.
+
+// Setup flags to activate the ISR PCINT1.
+// You may have to modify the next 2 lines if using other pins than A2 and A3
+//   PCICR |= (1 << PCIE1);    // This enables Pin Change Interrupt 1 that covers the Analog input pins or Port C.
+//   PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);  // This enables the interrupt for pin 2 and 3 of Port C.
+
+// The Interrupt Service Routine for Pin Change Interrupt 1
+// This routine will only be called on any signal change on A2 and A3.
+// ISR(PCINT1_vect) {
+//   encoder.tick(); // just call tick() to check the state.
+// }
+
+// The End
--- examples/SimplePollRotator/SimplePollRotator.ino
+++ examples/SimplePollRotator/SimplePollRotator.ino
@@ -1,18 +1,23 @@
 // -----
 // SimplePollRotator.ino - Example for the RotaryEncoder library.
 // This class is implemented for use with the Arduino environment.
+//
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// This work is licensed under a BSD 3-Clause License. See http://www.mathertel.de/License.aspx
 // More information on: http://www.mathertel.de/Arduino
 // -----
 // 18.01.2014 created by Matthias Hertel
+// 04.02.2021 conditions and settings added for ESP8266
 // -----
 
 // This example checks the state of the rotary encoder in the loop() function.
-// The current position is printed on output when changed.
+// The current position and direction is printed on output when changed.
 
 // Hardware setup:
-// Attach a rotary encoder with output pins to A2 and A3.
+// Attach a rotary encoder with output pins to
+// * A2 and A3 on Arduino UNO.
+// * D5 and D6 on ESP8266 board (e.g. NodeMCU).
+// Swap the pins when direction is detected wrong.
 // The common contact should be attached to ground.
 
 #include <Arduino.h>
@@ -23,7 +28,7 @@
 #define PIN_IN1 A2
 #define PIN_IN2 A3
 
-#else if defined(ESP8266)
+#elif defined(ESP8266)
 // Example for ESP8266 NodeMCU with input signals on pin D5 and D6
 #define PIN_IN1 D5
 #define PIN_IN2 D6
@@ -36,10 +41,10 @@
 // Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
 RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 
-
 void setup()
 {
-  Serial.begin(57600);
+  Serial.begin(115200);
+  while (! Serial);
   Serial.println("SimplePollRotator example for the RotaryEncoder library.");
 } // setup()
 
@@ -52,8 +57,10 @@ void loop()
 
   int newPos = encoder.getPosition();
   if (pos != newPos) {
+    Serial.print("pos:");
     Serial.print(newPos);
-    Serial.println();
+    Serial.print(" dir:");
+    Serial.println((int)(encoder.getDirection()));
     pos = newPos;
   } // if
 } // loop ()
