e48083e413ea047a36551f4e64232c1a3a792d54
Matthias Hertel <mathertel@hotmail.com>
2021-02-06 16:25:51 +0100

    Examples Update

--- examples/LimitedRotator/LimitedRotator.ino
+++ examples/LimitedRotator/LimitedRotator.ino
@@ -1,11 +1,13 @@
 // -----
 // LimitedRotator.ino - Example for the RotaryEncoder library.
 // This class is implemented for use with the Arduino environment.
+//
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// This work is licensed under a BSD 3-Clause License. See http://www.mathertel.de/License.aspx
 // More information on: http://www.mathertel.de/Arduino
 // -----
 // 26.03.2017 created by Matthias Hertel
+// 06.02.2021 conditions and settings added for ESP8266
 // -----
 
 // This example checks the state of the rotary encoder in the loop() function.
@@ -19,21 +21,38 @@
 // Attach a rotary encoder with output pins to A2 and A3.
 // The common contact should be attached to ground.
 
+#include <Arduino.h>
 #include <RotaryEncoder.h>
+
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
+// Example for Arduino UNO with input signals on pin 2 and 3
+#define PIN_IN1 2
+#define PIN_IN2 3
+
+#elif defined(ESP8266)
+// Example for ESP8266 NodeMCU with input signals on pin D5 and D6
+#define PIN_IN1 D5
+#define PIN_IN2 D6
+
+#endif
 
 #define ROTARYSTEPS 2
 #define ROTARYMIN 0
 #define ROTARYMAX 16
 
-// Setup a RoraryEncoder for pins A2 and A3:
-RotaryEncoder encoder(A2, A3);
+// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
+// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+
+// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 
 // Last known rotary position.
 int lastPos = -1;
 
 void setup()
 {
-  Serial.begin(57600);
+  Serial.begin(115200);
+  while (! Serial);
   Serial.println("LimitedRotator example for the RotaryEncoder library.");
   encoder.setPosition(10 / ROTARYSTEPS); // start with the value of 10.
 } // setup()
