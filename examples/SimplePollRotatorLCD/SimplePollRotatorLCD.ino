6c8b16d93f93355d7d2a0aed23242e47cc8d8aa9
Matthias Hertel <mathertel@hotmail.com>
2021-07-04 15:03:35 +0200

    fixing example

--- examples/SimplePollRotatorLCD/SimplePollRotatorLCD.ino
+++ examples/SimplePollRotatorLCD/SimplePollRotatorLCD.ino
@@ -20,8 +20,20 @@
 
 #include <RotaryEncoder.h>
 
-// Setup a RoraryEncoder for pins A2 and A3:
-RotaryEncoder encoder(A2, A3);
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
+// Example for Arduino UNO with input signals on pin 2 and 3
+#define PIN_IN1 2
+#define PIN_IN2 3
+
+#elif defined(ESP8266)
+// Example for ESP8266 NodeMCU with input signals on pin D5 and D6
+#define PIN_IN1 D5
+#define PIN_IN2 D6
+
+#endif
+
+// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
