0ddefcce24a9023db018e8e63d7851696211dd0b
José Henrique de Melo Luna <jhmluna@gmail.com>
2023-01-25 13:00:11 -0300

    Update InterruptRotator.ino
    Simplified example in InterruptRotator.ino

--- examples/InterruptRotator/InterruptRotator.ino
+++ examples/InterruptRotator/InterruptRotator.ino
@@ -28,40 +28,19 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 
-#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
 // Example for Arduino UNO with input signals on pin 2 and 3
-#define PIN_IN1 A2
-#define PIN_IN2 A3
-
-#elif defined(ESP8266)
-// Example for ESP8266 NodeMCU with input signals on pin D5 and D6
-#define PIN_IN1 D5
-#define PIN_IN2 D6
-
-#endif
+#define PIN_IN1 3
+#define PIN_IN2 2
 
 // A pointer to the dynamic created rotary encoder instance.
 // This will be done in setup()
 RotaryEncoder *encoder = nullptr;
 
-#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO_EVERY)
 // This interrupt routine will be called on any change of one of the input signals
 void checkPosition()
 {
   encoder->tick(); // just call tick() to check the state.
 }
-
-#elif defined(ESP8266)
-/**
- * @brief The interrupt service routine will be called on any change of one of the input signals.
- */
-IRAM_ATTR void checkPosition()
-{
-  encoder->tick(); // just call tick() to check the state.
-}
-
-#endif
-
 
 void setup()
 {
@@ -92,8 +71,6 @@ void loop()
 {
   static int pos = 0;
 
-  encoder->tick(); // just call tick() to check the state.
-
   int newPos = encoder->getPosition();
   if (pos != newPos) {
     Serial.print("pos:");
@@ -103,20 +80,5 @@ void loop()
     pos = newPos;
   } // if
 } // loop ()
-
-
-// To use other pins with Arduino UNO you can also use the ISR directly.
-// Here is some code for A2 and A3 using ATMega168 ff. specific registers.
-
-// Setup flags to activate the ISR PCINT1.
-// You may have to modify the next 2 lines if using other pins than A2 and A3
-//   PCICR |= (1 << PCIE1);    // This enables Pin Change Interrupt 1 that covers the Analog input pins or Port C.
-//   PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);  // This enables the interrupt for pin 2 and 3 of Port C.
-
-// The Interrupt Service Routine for Pin Change Interrupt 1
-// This routine will only be called on any signal change on A2 and A3.
-// ISR(PCINT1_vect) {
-//   encoder->tick(); // just call tick() to check the state.
-// }
 
 // The End
